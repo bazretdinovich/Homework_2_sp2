c4f299679d92901010264380cc59d7c4faaff119
Timur Ibragimov <bazretdinovich@gmail.com>
2025-02-23 23:18:48 +0500

    finish hw8

--- src/s2-homeworks/hw08/HW8.tsx
+++ src/s2-homeworks/hw08/HW8.tsx
@@ -32,7 +32,7 @@ const HW8 = () => {
     const [currentSort, setCurrentSort] = useState('')
 
     const finalPeople = people.map((u: UserType) => <User key={u._id} u={u}/>)
-
+    console.log(people)
     const sortUp = () => {
         setPeople(
             homeWorkReducer(initialPeople, {type: 'sort', payload: 'up'})
--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,14 +4,34 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
+            let sort: UserType[] = []
 
+            if (action.payload === 'up') {
+
+                sort = state.sort((a: UserType ,b: UserType): number => {
+                    if (a.name > b.name) return 1;
+                    if (a.name == b.name) return 0;
+                    if (a.name < b.name) return -1;
+                    return 0
+                })
+            } else {
+
+                sort = state.sort((a: UserType ,b: UserType): number => {
+                    if (a.name < b.name) return 1;
+                    if (a.name == b.name) return 0;
+                    if (a.name > b.name) return -1;
+                    return 0
+                })
+            }
+
+            state = sort
             return state // need to fix
         }
         case 'check': {
-
+            state = state.filter((el) => el.age >= action.payload)
             return state // need to fix
         }
         default:
